5ef9ee45ca287997bc5284e38396971ce8a2a5ef
alanchen365 <alanchen365@gmail.com>
2020-03-24 11:04:48 +0800

    初始化扩展包

---
+++ src/Base_old/BaseBean.php
@@ -0,0 +1,179 @@
+<?php
+namespace AsaEs\Base;
+
+use AsaEs\Utility\Tools;
+
+class BaseBean
+{
+    const FILTER_NOT_NULL  = 1;
+    const FILTER_NOT_EMPTY = 2; // 0 不算empty
+
+    /**
+     * 数据表名.
+     */
+    protected static $_tableName;
+
+    /**
+     * 表前缀
+     */
+    protected static $_prefix;
+
+    /**
+     * 主键
+     */
+    protected static $_primaryKey = 'id';
+
+    /**
+     * 获取所有列
+     * @return mixed
+     */
+    public function getFields()
+    {
+        return $this->_fields;
+    }
+
+    /**
+     * 获取所有列
+     * @param mixed $fields
+     */
+    public function setFields($fields): void
+    {
+        $this->_fields = $fields;
+    }
+
+    public function __construct(array $data = null, $autoCreateProperty = true)
+    {
+        // 数据填前做一些操作
+        $this->afterInitialize();
+
+        if ($data) {
+            $this->arrayToBean($data, $autoCreateProperty);
+        }
+    }
+
+    /**
+     * 数组转为Bean
+     * @param array $data
+     * @param bool  $autoCreateProperty
+     * @return SplBean
+     */
+    final public function arrayToBean(array $data, $autoCreateProperty = true): BaseBean
+    {
+        // 防止数据错乱 每一次赋值 都获取一个新的对象
+        $modelName = Tools::getModelNameByClass(get_called_class());
+        $newBeanInstance = new $modelName();
+
+        // 如果初始化为空 就返回一个空的bean
+        if (empty($data)) {
+            foreach ($this->_fields as $field) {
+                unset($newBeanInstance->$field);
+            }
+
+            return $newBeanInstance;
+        }
+
+        if ($autoCreateProperty == false) {
+            $data = array_intersect_key($data, array_flip($this->allProperty()));
+        }
+
+        /**
+         * 批量写数据
+         */
+        foreach ($this->_fields as $field) {
+            if (isset($data[$field])) {
+                $newBeanInstance->addProperty($field, $data[$field]);
+            }
+        }
+
+        // 数据填充完毕后之后做一些操作
+        $newBeanInstance->beforeInitialize();
+        return $newBeanInstance;
+    }
+
+    /**
+     * 添加成员
+     * @param string $name
+     * @param mixed  $value
+     */
+    final public function addProperty($name, $value = null): void
+    {
+        $this->$name = $value;
+    }
+
+    /**
+     * 数据填充前做一些操作
+     */
+    protected function beforeInitialize(): void
+    {
+    }
+
+    /**
+     * 数据填充后做一些操作
+     */
+    protected function afterInitialize():void
+    {
+    }
+    /**
+     * 输出为数组
+     * @param array|null $columns
+     * @param mixed      $filter
+     * @return array
+     */
+    function toArray(array $columns = null, $filter = null): array
+    {
+        $data = $this->jsonSerialize();
+        if ($columns) {
+            $data = array_intersect_key($data, array_flip($columns));
+        }
+        if ($filter === self::FILTER_NOT_NULL) {
+            return array_filter($data, function ($val) {
+                return !is_null($val);
+            });
+        } elseif ($filter === self::FILTER_NOT_EMPTY) {
+            return array_filter($data, function ($val) {
+                if ($val === 0 || $val === '0') {
+                    return true;
+                } else {
+                    return !empty($val);
+                }
+            });
+        } elseif (is_callable($filter)) {
+            return array_filter($data, $filter);
+        }
+        unset($data['_fields']);
+        return $data;
+    }
+
+    /**
+     * 序列化
+     * @return array
+     */
+    final public function jsonSerialize(): array
+    {
+        // TODO: Implement jsonSerialize() method.
+        $data = [];
+        foreach ($this as $key => $item) {
+            $data[$key] = $item;
+        }
+        return $data;
+    }
+
+    public function __call($actionName, $arguments){
+        return null;
+    }
+
+    /**
+     * 对private变量可写
+     * @param string $name
+     * @param $value
+     */
+    public function __set(string $name, $value): void
+    {
+        $this->$name = $value;
+    }
+
+    public function &__get(string $name)
+    {
+        return $this->$name;
+    }
+}
---
+++ src/Base_old/BaseService.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace AsaEs\Base;
+
+use AsaEs\Exception\AppException;
+use AsaEs\Proxy\DaoProxy;
+
+class BaseService
+{
+    public function __call($actionName, $arguments)
+    {
+        if ($this->daoObj instanceof DaoProxy) {
+            $ref = new \ReflectionClass($this->daoObj->getClass());
+            if ($ref->hasMethod($actionName) &&  $ref->getMethod($actionName)->isPublic() && !$ref->getMethod($actionName)->isStatic()) {
+                return call_user_func_array([$this->daoObj,$actionName], $arguments);
+            } else {
+                throw new AppException(1013, "你调用了service中的{$actionName}()方法,但它不存在");
+            }
+        }
+    }
+
+    /**
+     * dao对象
+     *
+     * @var
+     */
+    protected $daoObj;
+
+    /**
+     * @return mixed
+     */
+    public function getDaoObj()
+    {
+        return $this->daoObj;
+    }
+
+    /**
+     * @param mixed $daoObj
+     */
+    public function setDaoObj($daoObj): void
+    {
+        // 这里先走代理
+        $this->daoObj = new DaoProxy($daoObj);
+    }
+
+    /**
+     * 按列删除
+     * @param array $fieldValues
+     */
+    public function deleteByField(array $fieldValues) :void
+    {
+        $this->getDaoObj()->deleteByField($fieldValues);
+    }
+
+    /**
+     * 获取单条
+     * @param int|null $id
+     * @return mixed
+     */
+    public function getById(?int $id)
+    {
+        return $this->getDaoObj()->getById($id);
+    }
+
+    /**
+     * 根据某列获取单条
+     * @param array $params
+     * @param array $searchLinkType
+     * @param array $page
+     * @param array $orderBys
+     * @param array $groupBys
+     * @return mixed
+     */
+    public function getOneByField(array $params = [], array $searchLinkType = [], array $page = [], array $orderBys = [], array $groupBys = [])
+    {
+        return  $this->getDaoObj()->getOneByField($params, $searchLinkType, $page, $orderBys, $groupBys);
+    }
+
+    /**
+     * 查列表
+     * @param array $params
+     * @param array $searchLinkType
+     * @param array $page
+     * @param array $orderBys
+     * @param array $groupBys
+     * @return array
+     */
+    public function getAll(array $params = [], array $searchLinkType = [], array $page = [], array $orderBys = [], array $groupBys = []): array
+    {
+        return  $this->getDaoObj()->getAll($params, $searchLinkType, $page, $orderBys, $groupBys);
+    }
+
+    /**
+     * 搜索
+     * @param array $params
+     * @param array $searchLinkType
+     * @param array $page
+     * @param array $orderBys
+     * @param array $groupBys
+     * @return array
+     */
+    public function searchAll(array $params = [], array $searchLinkType = [], array $page = [], array $orderBys = [], array $groupBys = []): array
+    {
+        return  $this->getDaoObj()->searchAll($params, $searchLinkType, $page, $orderBys, $groupBys);
+    }
+
+    /**
+     * 根据id删除
+     * @param array $ids
+     */
+    public function deleteByIds(array $ids):void
+    {
+        $this->getDaoObj()->deleteByIds($ids);
+    }
+
+    /**
+     * 批量插入
+     * @param array $params
+     * @return array
+     */
+    public function insertAll(array $params): array
+    {
+        return $this->getDaoObj()->insertAll($params);
+    }
+
+    /**
+     * 批量插入忽略错误
+     * @param array $params
+     * @return array
+     */
+    public function insertAllIgnoreErr(array $params): void
+    {
+        $this->getDaoObj()->insertAllIgnoreErr($params);
+    }
+
+    /**
+     * 插入单条数据
+     * @param array $params
+     */
+    public function insert(array $params) :int
+    {
+        return $this->getDaoObj()->insert($params);
+    }
+
+    /**
+     * 更新全表中某列等于某个值的所有数据.
+     *
+     * @param string $field
+     * @param string $value
+     *
+     * @throws MysqlException
+     */
+    public function updateByField(array $originalFieldValues, array $updateFieldValues): void
+    {
+        $this->getDaoObj()->updateByField($originalFieldValues, $updateFieldValues);
+    }
+
+    /**
+     * 根据id更新
+     * @param array $ids
+     * @param array $params
+     * @return mixed
+     */
+    public function updateByIds(array $ids, array $params)
+    {
+        return $this->getDaoObj()->updateByIds($ids, $params);
+    }
+
+    /**
+     * 截断表
+     * @return mixed
+     */
+    public function truncate()
+    {
+        return $this->getDaoObj()->truncate();
+    }
+}
