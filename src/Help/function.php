5ea1074f9d4baa812605dd8642d484252fc7f51d
alanchen365 <alanchen365@gmail.com>
2020-09-17 08:44:42 +0800

    更新

--- src/Help/function.php
+++ src/Help/function.php
@@ -11,3 +11,8 @@ function isDev(): bool
     $env = \Es3\EsConfig::getInstance()->getConf('ENV');
     return $env === strtolower('PRODUCTION') ? false : true;
 }
+
+function config($keyPath = '', $env = false)
+{
+    return \Es3\EsConfig::getInstance()->getConf($keyPath, $env);
+}
